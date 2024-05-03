c597356d5fa03154ebb6d93021b005a1e44405b3
Nicolasgosu <71670329+Nicolasgosu@users.noreply.github.com>
2024-05-03 15:13:30 -0500

    Update Codigo_maestro.ino

--- Codigo_maestro.ino
+++ Codigo_maestro.ino
@@ -9,27 +9,27 @@ void setup() {
 }
 
 void loop() {
-  float temperatura;
+  float luminosidad;
 
   // Solicita datos al esclavo
-  Wire.requestFrom(8, sizeof(temperatura));
+  Wire.requestFrom(8, sizeof(luminosidad));
   
   // Espera a que los datos estén disponibles
-  while (Wire.available() < sizeof(temperatura));
+  while (Wire.available() < sizeof(luminosidad));
   
   // Lee los datos del esclavo
-  Wire.readBytes((byte*)&temperatura, sizeof(temperatura));
+  Wire.readBytes((byte*)&temperatura, sizeof(luminosidad));
   
-  // Imprime la temperatura recibida por la consola serial
+  // Imprime la luminosidad recibida por la consola serial
   
   Serial.println(temperatura);
   
   
   // Realiza alguna acción basada en los datos recibidos
-  if (temperatura > 25.0) {
-    digitalWrite(LED_PIN, HIGH); // Enciende el LED si la temperatura es mayor a 25°C
+  if (luminosidad > 30) {
+    digitalWrite(LED_PIN, HIGH); // Enciende el LED si la luminosidad es mayor a 30 lumens
   } else {
-    digitalWrite(LED_PIN, LOW); // Apaga el LED si la temperatura es menor o igual a 25°C
+    digitalWrite(LED_PIN, LOW); // Apaga el LED si la la luminosidad es menor a 30 lumens
   }
 
   delay(1000); // Espera 1 segundo antes de solicitar nuevamente los datos

7682e202f0e0c88e8b73bf55add4c6a079975d15
Nicolasgosu <71670329+Nicolasgosu@users.noreply.github.com>
2024-05-03 17:55:37 -0500

    Update Taller.py
    se añadieron comentarios

--- Taller.py
+++ Taller.py
@@ -2,18 +2,18 @@ import serial
 import thingspeak
 
 
-puerto_serial = serial.Serial('COM6', 9600)  
-canal = thingspeak.Channel(id='[replace by id]', api_key='[Replace by key]')
+puerto_serial = serial.Serial('COM6', 9600)  #se inicializa comunicacion serial con el arduino
+canal = thingspeak.Channel(id='[replace by id]', api_key='[Replace by key]') #se inicializa la conexion con el canal de ThingSpeak 
 
 try:
     while True:
         
-        linea = puerto_serial.readline().decode().strip()
+        linea = puerto_serial.readline().decode().strip() #se lee la comunicacion serial
     
         if linea:
-            canal.update({1: linea})
+            canal.update({1: linea}) #en caso de una lectura exitosa se actualiza el canal de ThingSpeak
         print(linea)
 
 finally:
     
-    puerto_serial.close()
+    puerto_serial.close() #se cierra la comunicacion
